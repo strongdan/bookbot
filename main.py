72ebb2c298591409335ecfcda30c6230a4fe8686
Daniel Strong <danielstrong@mac.com>
2025-06-26 11:14:47 -0800

    modify to print stats for any book

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-from stats import get_word_length, count_characters
+from stats import get_word_count, count_characters, sort_dictionary_list
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    filepath = sys.argv[1]
 
 def get_book_text(filepath):
   with open(filepath) as f:
-    file_contents = f.read()
-    return file_contents
-
-character_count = count_characters(get_book_text('./books/frankenstein.txt'))
+    return f.read()
 
 def main():
-    print(get_word_length(get_book_text('./books/frankenstein.txt')))
-    print(character_count)
+    file_contents = get_book_text(filepath)
+    word_count = get_word_count(file_contents)
+    character_count = count_characters(file_contents)
+    sorted_list = sort_dictionary_list(character_count)
+
+    character_lines = "\n".join([f"{item['char']}: {item['num']}" for item in sorted_list])
+
+    print(f'''============ BOOKBOT ============
+    Analyzing book found at {filepath}...
+    ----------- Word Count ----------
+    Found {word_count} total words
+    --------- Character Count -------
+    {character_lines}
+    ============= END ===============''')
 
 main()
--- stats.py
+++ stats.py
@@ -1,11 +1,24 @@
-def get_word_length(file_contents):
-    return str(len(file_contents.split())) + " words found in the document"
+def get_word_count(file_contents):
+    return str(len(file_contents.split()))
 
 def count_characters(file_contents):
-    character_counts = {}
-    for character in list(file_contents):
-        if character.lower() in character_counts:
-            character_counts[character.lower()] += 1
-        else:
-            character_counts[character.lower()] = 1
-    return character_counts 
+    character_counts = []
+    for character in file_contents:
+        char = character.lower()
+        if char.isalpha():
+            for d in character_counts:
+                if d['char'] == char:
+                    d['num'] += 1
+                    break
+            else:
+                character_counts.append({'char': char, 'num': 1})
+    return character_counts
+
+def sort_on(item):
+    return item["num"]
+
+# takes the dictionary of characters and their counts 
+# and returns a sorted list of dictionaries
+def sort_dictionary_list(character_dictionary_list):
+    sorted_list = sorted(character_dictionary_list, reverse=True, key=sort_on)
+    return sorted_list
