3976a44fbda6138044da33691bfb5f79797bd0f0
Daniel Strong <danielstrong@mac.com>
2025-06-26 09:35:10 -0800

    add a function that returns word count

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ def get_book_text(filepath):
     file_contents = f.read()
     return file_contents
 
+def get_word_length(file_contents):
+    return str(len(file_contents.split())) + " words found in the document"
+
 def main():
-    print(get_book_text('./books/frankenstein.txt'))
-    
+    print(get_word_length(get_book_text('./books/frankenstein.txt')))
+
 main()
